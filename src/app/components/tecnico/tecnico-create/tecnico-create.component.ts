497aa12a66f1f911369bc5153fd8c3257b0ce13f
antonio.rocha <antonio.rocha@quality.com.br>
2023-07-24 15:30:11 -0300

    adição de validações nos campos e ngx-mask adicionado

--- src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-tecnico-create',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-create.component.css']
 })
 export class TecnicoCreateComponent implements OnInit {
+  
+  nome: FormControl = new FormControl(null, Validators.minLength(3));
+  cpf: FormControl = new FormControl(null, Validators.required);
+  email: FormControl = new FormControl(null, Validators.email);
+  senha: FormControl = new FormControl(null, Validators.minLength(3));
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-}
+  validaCampos(): boolean {
+    return this.nome.valid && this.cpf.valid 
+    && this.email.valid && this.senha.valid;
+  }
+} 
